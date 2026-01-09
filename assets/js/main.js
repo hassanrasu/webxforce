e551affdeb3ca04dccee23814b02194fb3878a4d
Hasan Rakesh <85984765+HasanRakesh@users.noreply.github.com>
2026-01-09 19:36:03 +0600

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,100 @@
     "use strict";
     jQuery(document).ready(function ($) {
 
+        
+        	$('.brand-logo-slider').slick({
+            dots: false,
+            arrows: true,
+            speed: 300,
+            slidesToShow: 8,
+            slidesToScroll: 1,
+            centerMode: true,
+            autoplay: true,
+                cssEase: 'linear',
+            infinite: true,
+            autoplaySpeed: 5000,
+            prevArrow: false,
+            nextArrow: false,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 750,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+
+              ]
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     });
 }(jQuery));
