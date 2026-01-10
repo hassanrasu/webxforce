aa2005dc6ce314b191c09fcb7c068d819c6f65a3
Hasan Rakesh <85984765+HasanRakesh@users.noreply.github.com>
2026-01-10 22:46:33 +0600

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,6 +87,59 @@
         });
 
         
+         	$('.testimonial-slider').slick({
+            dots: false,
+            arrows: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: true,
+            autoplay: true,
+                cssEase: 'linear',
+            infinite: true,
+            autoplaySpeed: 5000,
+            prevArrow: false,
+            nextArrow: false,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 750,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+
+              ]
+        });
         
         
         
